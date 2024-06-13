1898499558666020c768866618c33b42eb92e34b
Ashkan <ashkantavangar7@gmail.com>
2024-06-13 23:46:45 +0330

    Create templates and config them in settings

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'blog_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
