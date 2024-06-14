516e7abbcd1fe57f01975c9109dd489716203d89
Ashkan <ashkantavangar7@gmail.com>
2024-06-15 01:29:50 +0330

    Create  views for GET method of login , and register

--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.shortcuts import render
+
+def users_login_view(request):
+    form = AuthenticationForm()
+    return render(request,"users/users_login.html",{"form":form})
+
+def users_register_view(request):
+    form = UserCreationForm()
+    return render(request,"users/users_register.html",{"form" : form})
 
 
-
-def users_view(request):
-    return HttpResponse("HELLO USER")
+def users_logout_view(request):
+    pass
