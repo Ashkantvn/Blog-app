aebe53ffa3d6a30afe126af34282dc009bdcde4a
Ashkan <ashkantavangar7@gmail.com>
2024-06-15 23:48:20 +0330

    Add path for media

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    re_path(r"^media/(?P<path>.*)$", serve,{'document_root' : settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve,{'document_root' : settings.STATIC_ROOT}),
 
     path('admin/', admin.site.urls),
