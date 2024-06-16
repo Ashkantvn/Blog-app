e991e6bdb5eedff8b713112f3d8fc3fd84bb274b
Ashkan <ashkantavangar7@gmail.com>
2024-06-16 19:00:27 +0330

    Update urls

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name="posts"
 
 urlpatterns = [
     path("",views.posts_list_view,name="list"),
-    path("details/",views.posts_details_view,name="details"),
+    path("<int:pk>",views.posts_details_view,name="details"),
     path("edit/",views.posts_edit_view,name="edit"),
     path("add/",views.posts_add_view,name="add"),
     path("delete/",views.posts_delete_view,name="delete"),
