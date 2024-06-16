6e03afcee971ac95b77c5a19c9395a1807375c77
Ashkan <ashkantavangar7@gmail.com>
2024-06-16 19:00:50 +0330

    Create post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    slug = models.SlugField(max_length=60)
+    banner = models.ImageField(default='fallback.png', blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, default=None , on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.title)
+       super(Post, self).save(*args, **kwargs) # Call the real save() method
+    
