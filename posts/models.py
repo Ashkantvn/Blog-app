b9b3bf5713b78324eb3d803c66c909be769f4414
Ashkan <ashkantavangar7@gmail.com>
2024-06-17 18:59:52 +0330

    add get absolute url function into the posts model

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -18,4 +19,9 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
        self.slug = slugify(self.title)
        super(Post, self).save(*args, **kwargs) # Call the real save() method
+
+
+    def get_absolute_url(self):
+        return reverse("posts:details", kwargs={"pk": self.pk})
+    
     
