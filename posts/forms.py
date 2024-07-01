0fe20e7af15c8d0a476be158e0619dbccdcfe66a
Ashkan <ashkantavangar7@gmail.com>
2024-07-01 22:09:38 +0330

    Create form for comments model

--- posts/forms.py
+++ posts/forms.py
@@ -6,3 +6,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = models.Post
         fields = ["title","content","banner"]
+
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model = models.Comment
+        fields = ["content"]
