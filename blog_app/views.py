8d7d8e133b8829ba19c8a26c224c06ae59b3f232
Ashkan <ashkantavangar7@gmail.com>
2024-07-07 12:42:19 +0330

    Add recent comments in home page template and template

--- blog_app/views.py
+++ blog_app/views.py
@@ -7,7 +7,11 @@ from posts import models
 
 def home_view(request):
     latest_posts = models.Post.objects.all().order_by('-date_created')[:9]
-    context = {"latest_posts":latest_posts}
+    comments = models.Comment.objects.all().order_by("-created_date")[:9]
+    context = {
+        "latest_posts":latest_posts,
+        "comments":comments,    
+    }
     return render(request,"home.html",context)
 
 
