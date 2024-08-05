c1ab65274a15a1d5cc6625b4ce857f6978f130b3
Ashkan <ashkantavangar7@gmail.com>
2024-08-05 15:04:19 +0330

    Update home view to detect comment content language

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from posts import models
 from django.conf import settings
 from django.utils import translation
+from lingua import LanguageDetectorBuilder
 
 
 
 
 
 def home_view(request):
+    detector = LanguageDetectorBuilder.from_all_languages().build()
     latest_posts = models.Post.objects.all().order_by('-date_created')[:9]
     comments = models.Comment.objects.all().order_by("-created_date")[:9]
+    for comment in comments:
+        lang = detector.detect_language_of(comment.comment_for.content).iso_code_639_1.name.lower()
+        comment.lang = lang
     context = {
         "latest_posts":latest_posts,
         "comments":comments,   
