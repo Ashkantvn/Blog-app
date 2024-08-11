ae99e89813810376d7380a0eb0b822c3519c1c3e
Ashkan <ashkantavangar7@gmail.com>
2024-08-11 12:49:16 +0330

    Create test for urls in podcast

--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse,resolve
+from . import views
 
 # Create your tests here.
+
+# url tests
+class testPodcastURL(SimpleTestCase):
+
+    def test_podcast_list_is_resolve(self):
+        url = reverse("podcasts:list")
+        self.assertEqual(resolve(url).func,views.podcast_list)
