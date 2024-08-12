ac973eedf4a4ff2f2ebe59867c35e48d393df6f5
Ashkan <ashkantavangar7@gmail.com>
2024-08-12 23:59:03 +0330

    Add paginator in view and template and add podcast list in template

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render,HttpResponse
 from .models import Podcast
-
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 # Create your views here.
 
 # podcast list view
 def podcast_list(request):
     podcasts = Podcast.objects.all()
+    paginator = Paginator(podcasts,10)
+    current_page = request.GET.get('page',1)
+    try:
+        paginator_result = paginator.page(current_page)
+    except PageNotAnInteger:
+        paginator_result = paginator.page(1)
+    except EmptyPage:
+        paginator_result = paginator.page(paginator.num_pages)
     context = {
-        "podcasts":podcasts
+        "podcasts":paginator_result
     }
     return render(request,'podcasts/podcasts_list.html',context)
