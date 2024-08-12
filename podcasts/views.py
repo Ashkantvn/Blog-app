dc9ba19962cb85bef4405fa4a91c537e94c1ac63
Ashkan <ashkantavangar7@gmail.com>
2024-08-12 13:56:42 +0330

    Create view and template of podcast list

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from .models import Podcast
 
 # Create your views here.
 
 # podcast list view
 def podcast_list(request):
-    return HttpResponse("PODCAST")
+    podcasts = Podcast.objects.all()
+    context = {
+        "podcasts":podcasts
+    }
+    return render(request,'podcasts/podcasts_list.html',context)
