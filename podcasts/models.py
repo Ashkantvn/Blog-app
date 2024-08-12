6f6fcbf51f8ce8f998188c4541216dae9befe91d
Ashkan <ashkantavangar7@gmail.com>
2024-08-12 10:58:40 +0330

    Create podcast model and migrations

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.urls import reverse
+
 
 # Create your models here.
 
 class Podcast(models.Model):
-    pass
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    banner = models.ImageField(default='fallback.png',blank=True)
+    audio = models.FileField(upload_to='audio/')
+    podcaster = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.title)
+       super(Podcast, self).save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse("podcasts:details", kwargs={"pk": self.pk})
