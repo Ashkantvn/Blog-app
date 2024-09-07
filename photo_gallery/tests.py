7c365d82ff6618d983fce7bee538d05e1c997883
Ashkan <ashkantavangar7@gmail.com>
2024-09-07 14:09:18 +0330

    Create test for views

--- photo_gallery/tests.py
+++ photo_gallery/tests.py
@@ -10,3 +10,12 @@ class PhotoGalleryUrlTest(SimpleTestCase):
     def test_photo_gallery_list_is_resolved(self):
         url = reverse('photoGallery:list')
         self.assertEqual(resolve(url).func,views.gallery_view)
+
+
+#views tests
+class PhotoGalleryViewsTest(TestCase):
+
+    def test_photo_gallery_list_views(self):
+        response = self.client.get(reverse('photoGallery:list'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'photo_gallery/list.html')
