a9e6a16b6abf88773b3622c496b488fd536ce080
Ashkan <ashkantavangar7@gmail.com>
2024-09-11 11:50:32 +0330

    photo gallery posts updates

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 # Create your views here.
 def gallery_view(request):
     return render(request,'photo_gallery/list.html')
 
 
 def post_gallery(request):
-    return render(request,'photo_gallery/posts.html')
+    posts = Post.objects.all()
+    context = {
+     'posts':posts   
+    }
+    return render(request,'photo_gallery/posts.html',context)
