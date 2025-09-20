b7f9bc13beb0de5b287ba03496fabf97d3464c93
Ashkan <ashkantavangar7@gmail.com>
2025-09-20 12:45:41 +0330

    Recreate Blog-app

--- photo_gallery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name="photoGallery"
-urlpatterns = [
-    path("", views.gallery_view, name="list"),
-    path("posts", views.post_gallery , name="posts"),
-    path("podcasts", views.podcast_gallery, name="podcasts")
-]
