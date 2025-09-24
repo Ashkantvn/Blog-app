c8cd9cb262174631dbbd94c933c707b60132362d
Ashkan <ashkantavangar7@gmail.com>
2025-09-24 20:35:26 +0330

    initializer celery and smtp4dev

--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -125,3 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
+
+# Celery
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
