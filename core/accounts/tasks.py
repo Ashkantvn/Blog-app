19bcfcd123bf9b336a2f46c74a023768e25292a0
Ashkan <ashkantavangar7@gmail.com>
2025-09-26 18:37:15 +0330

    Add celery beat to project

--- core/accounts/tasks.py
+++ core/accounts/tasks.py
@@ -1,32 +1,39 @@
 from celery import shared_task
 from django.core.mail import send_mail
-import random, string
 from accounts.models import ConfirmCode
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.conf import settings
+from accounts.email_templates import CONFIRM_CODE_TEMPLATE
+from accounts.utils import generate_random_string
+
+from django.utils import timezone
+from datetime import timedelta
 
 User = get_user_model()
-
-def generate_random_string(length=12):
-    characters = string.ascii_letters + string.digits  # a-zA-Z0-9
-    return ''.join(random.choices(characters, k=length))
 
 @shared_task
 def send_confirm_code_mail(email):
     user = User.objects.filter(email=email).first()
     code = generate_random_string(12)
-    domain = 'http://localhost:8000' # Change it in production
-    reset_confirm_url = reverse("accounts:reset-confirm",args=[code])
-    ConfirmCode.objects.create(
-        code=code,
+    ConfirmCode.objects.update_or_create(
         user=user,
+        defaults={"code":code},
+    )
+    message = CONFIRM_CODE_TEMPLATE.format(
+        code=code
     )
     # Send mail
     send_mail(
         subject='Your confirm code!',
-        message= domain + reset_confirm_url,
+        message= message,
         from_email='noreply@example.com',
         recipient_list=[email],
         fail_silently=False,
     )
+
+@shared_task
+def delete_expired_confirm_codes():
+    target_time = timezone.now() - timedelta(minutes=3)
+    ConfirmCode.objects.filter(
+        created_date__lt=target_time
+    ).delete()
---
+++ core/accounts/utils.py
@@ -0,0 +1,5 @@
+import random, string
+
+def generate_random_string(length=12):
+    characters = string.ascii_letters + string.digits  # a-zA-Z0-9
+    return ''.join(random.choices(characters, k=length))
