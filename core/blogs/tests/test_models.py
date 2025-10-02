d4467e8c97d8204658986b06c18b4308f813ffac
Ashkan <ashkantavangar7@gmail.com>
2025-10-02 22:32:54 +0330

    Update model test and create blog details url test

--- core/blogs/tests/test_models.py
+++ core/blogs/tests/test_models.py
@@ -11,13 +11,25 @@ class TestblogModel:
         assert blog.content == "This is a test blog content."
         assert blog.banner == "blogs/default.png"
         assert blog.views == 0
-        assert isinstance(blog.tags, Tag)
-        assert blog.is_published is False
-        assert blog.publishable is False
-        assert isinstance(blog.author, User)
+        assert isinstance(blog.tags.first(), Tag),(
+            "tags is not a Tag instance"
+        )
+        assert blog.is_published is False,(
+            "is_published should be False by default"
+        )
+        assert blog.publishable is False,(
+            "publishable should be False by default"
+        )
+        assert isinstance(blog.author, User),(
+            "author is not a User instance"
+        )
         assert blog.blog_slug == "test-blog"
-        assert blog.created_date is not None
-        assert blog.updated_date is not None
+        assert blog.created_date is not None,(
+            "created_date is not set"    
+        )
+        assert blog.updated_date is not None,(
+            "updated_date is not set"
+        )
 
 
 
@@ -32,11 +44,15 @@ class TestblogModel:
 @pytest.mark.django_db
 class TestTagModel:
     def test_tag_creation(self, tag):
-        assert tag.name == "Django"
+        assert tag.tag_name == "Django"
         assert tag.tag_slug == "django"
-        assert tag.created_date is not None
-        assert tag.updated_date is not None
+        assert tag.created_date is not None,(
+            "created_date is not set"
+        )
+        assert tag.updated_date is not None,(
+            "updated_date is not set"    
+        )
 
 
     def test_tag_str(self, tag):
-        assert str(tag) == tag.name
+        assert str(tag) == tag.tag_name
---
+++ core/blogs/tests/test_urls.py
@@ -0,0 +1,11 @@
+import pytest
+from django.urls import reverse, resolve
+from blogs import views
+
+@pytest.mark.django_db
+class TestBlogURLs:
+    def test_blog_details_is_resolved(self):
+        url = reverse("blogs:details", kwargs={"blog_slug": "blog_slug"})
+        view_class = resolve(url).func.view_class
+        assert view_class == views.BlogDetail
+        assert url == "/blogs/blog_slug/"
