4fe49214783f13fe14123ccbdbaf487c39277527
Ashkan <ashkantavangar7@gmail.com>
2025-10-02 23:52:54 +0330

    Create urls of blogs

--- core/blogs/views/__init__.py
+++ core/blogs/views/__init__.py
@@ -1,1 +1,2 @@
-from .blog_display import BlogDetails
+from .blog_display import BlogDetails, BlogList
+from .blog_management import BlogAdd, BlogEdit, BlogDelete
---
+++ core/blogs/views/blog_management.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+
+class BlogAdd(View):
+    pass
+
+class BlogEdit(View):
+    pass
+
+class BlogDelete(View):
+    pass
