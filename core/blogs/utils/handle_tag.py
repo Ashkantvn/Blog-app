81ec0bbca2c15b63733e82905214b4e304f30f7a
Ashkan <ashkantavangar7@gmail.com>
2025-10-03 21:37:27 +0330

    Update handle tag method

--- core/blogs/utils/handle_tag.py
+++ core/blogs/utils/handle_tag.py
@@ -6,5 +6,7 @@ def handle_tags(blog_instance, tags):
         blog_instance.tags.clear()
         tag_list = [tag.strip() for tag in tags.split(',')]
         for tag_name in tag_list:
-            tag_obj = Tag.objects.get_or_create(tag_slug=slugify(tag_name))[0]
+            tag_obj = Tag.objects.get_or_create(tag_slug=slugify(tag_name),defaults={"tag_name":tag_name})[0]
+            tag_obj.tag_name = tag_name
+            tag_obj.save()
             blog_instance.tags.add(tag_obj)
