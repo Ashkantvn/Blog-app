06ae6567fd1bc43e07a7ef3d83d69514390ef93d
Ashkan <ashkantavangar7@gmail.com>
2025-10-05 23:19:13 +0330

    Update home view

--- core/core/views.py
+++ core/core/views.py
@@ -1,10 +1,25 @@
 from django.views import View
 from django.shortcuts import render
+from blogs.models import Blog
+from django.utils.timezone import now
 
 class Home(View):
     
     def get(self, request):
+        blogs = Blog.objects.filter(
+            is_published=True,
+            publishable=True,
+            published_date__lte=now().date()
+        )
+        latest_blogs = blogs.order_by("-published_date")
+        most_view_blogs = blogs.order_by("-views")
         return render(
             request,
             "core/home.html",
+            {
+                "data":{
+                    "latest_blogs":latest_blogs,
+                    "most_view_blogs":most_view_blogs
+                },
+            },
         )
