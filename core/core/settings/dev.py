6360ff94bc3d01f31417c521de31c307ac4d6691
ashkan <ashkantavangar7@gmail.com>
2025-11-13 14:09:59 +0330

    Update settings and configs

--- core/core/settings/dev.py
+++ core/core/settings/dev.py
@@ -1,5 +1,10 @@
 from .base import *
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp4dev"
 EMAIL_PORT = 25
@@ -7,4 +12,3 @@ EMAIL_USE_TLS = False
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
